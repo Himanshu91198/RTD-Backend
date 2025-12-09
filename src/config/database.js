cb1a29a6168d50cb11db7f56ab17d58b699d6a44
Himanshu Auti <himanshuauti98@gmail.com>
2025-12-09 13:15:23 +0530

    1. Added changes in database,js

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
   } catch (err) {
     console.log("MongoDB connection failed", err);
     process.exit(1);
